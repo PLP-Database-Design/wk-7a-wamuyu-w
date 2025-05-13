5579e469671d208aad33187de9b1a80193cc027c
Wamuyu 💜 <106072185+wamuyu-w@users.noreply.github.com>
2025-05-13 11:11:26 +0300

    Question 2 done

--- answers.sql
+++ answers.sql
@@ -37,4 +37,29 @@ FROM split_cte
 ORDER BY OrderID;
 
 -- Question 2
+--Original table
+CREATE TEMPORARY TABLE OrderDetails (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(100),
+    Quantity INT
+);
+
+INSERT INTO OrderDetails (OrderID, CustomerName, Product, Quantity) VALUES
+(101, 'John Doe', 'Laptop', 2),
+(101, 'John Doe', 'Mouse', 1),
+(102, 'Jane Smith', 'Tablet', 3),
+(102, 'Jane Smith', 'Keyboard', 1),
+(102, 'Jane Smith', 'Mouse', 2),
+(103, 'Emily Clark', 'Phone', 1);
+
+--Extract Orders
+CREATE TEMPORARY TABLE Orders AS
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+-- Extract OrderItems
+CREATE TEMPORARY TABLE OrderItems AS
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
 
